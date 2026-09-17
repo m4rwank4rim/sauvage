41e5ab7ce012aaefbc9197e9ad0c0149b7a3e42a
admin <admin@local.test>
2026-09-17 07:01:25 +0100

    Fix fonts: remove circular --font-inter/--font-display self-references in globals.css; drop temp check scripts

--- check-font.js
+++
@@ -1,9 +0,0 @@
-fetch('https://sauvage-kohl.vercel.app')
-  .then(r => r.text())
-  .then(t => {
-    const m = t.match(/<html[^>]*class="([^"]*)"/);
-    console.log('HTML class:', m ? m[1] : 'none');
-    const f = t.match(/font-family[^;]*var\(--font-display\)[^;]*/g);
-    console.log('font-display refs:', f);
-  })
-  .catch(console.error);
